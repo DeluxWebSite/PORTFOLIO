b88af5f9b178f5d6623bba56a5b93f6c66500e6e
Serge_M <sergey.meljnick@yandex.ru>
2022-12-29 02:23:41 +0300

    delete umtsk

--- umtsk/footer.php
+++
@@ -1,7 +0,0 @@
-<?php get_sidebar('footer'); ?>
-<footer class="footer">
-    <small class="copyright">
-        &copy; 2020-2022 Copyright by Serge Meljnick for gold customer
-        <i class="fa fa-heart"></i>
-    </small>
-</footer>
